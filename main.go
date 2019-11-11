c78c17979342b878ceca54fa1247e04377d0b20c
Salt Dev 3 <saltdev3@appliedtechnology.se>
2019-11-11 21:57:00 +0100

    get one product api completed

--- main.go
+++ main.go
@@ -6,6 +6,8 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"reflect"
+	"time"
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
@@ -20,13 +22,44 @@ type Item struct {
 	Title     string `json:"title"`
 }
 
-// Post is for the posts
-type Post struct {
+// GetAllProducts is for all products
+type GetAllProducts struct {
 	TotalCount int    `json:"totalCount"`
 	Items      []Item `json:"items"`
 }
 
-func get(w http.ResponseWriter, r *http.Request) {
+// Attribute is for a specific attribute
+type Attribute struct {
+	Name  string `json:"name"`
+	Value string `json:"value"`
+}
+
+// GetProduct is for one product only
+type GetProduct struct {
+	ProductID   int         `json:"productId"`
+	Title       string      `json:"title"`
+	Sku         string      `json:"sku"`
+	Barcodes    []string    `json:"barcodes"`
+	Attributes  []Attribute `json:"attributes"`
+	Price       float64     `json:"price"`
+	Created     time.Time   `json:"created"`
+	LastUpdated time.Time   `json:"lastUpdated"`
+}
+
+// HasElem is a funciton to test deep equality of structs
+func HasElem(s interface{}, elem interface{}) bool {
+	arrV := reflect.ValueOf(s)
+	if arrV.Kind() == reflect.Slice {
+		for i := 0; i < arrV.Len(); i++ {
+			if reflect.DeepEqual(arrV.Index(i).Interface(), elem) {
+				return true
+			}
+		}
+	}
+	return false
+}
+
+func getProducts(w http.ResponseWriter, r *http.Request) {
 	v := r.URL.Query()
 
 	start := v.Get("start")
@@ -46,23 +79,26 @@ func get(w http.ResponseWriter, r *http.Request) {
 		barcode = "%"
 	}
 
-	var post Post
+	var getAllProducts GetAllProducts
 	var items []Item
 
 	countRes, err := db.Query("SELECT COUNT(product_id) FROM sitoo_test_assignment.product")
 	if err != nil {
-		log.Fatal(err)
+		errorHandler(w, r, http.StatusInternalServerError)
+		// log.Fatal(err)
 	}
 	defer countRes.Close()
 	for countRes.Next() {
-		if err := countRes.Scan(&post.TotalCount); err != nil {
-			log.Fatal(err)
+		if err := countRes.Scan(&getAllProducts.TotalCount); err != nil {
+			// log.Fatal(err)
+			errorHandler(w, r, http.StatusInternalServerError)
 		}
 	}
 
 	itemRes, err := db.Query("SELECT p.product_id, p.title FROM sitoo_test_assignment.product p LEFT JOIN sitoo_test_assignment.product_barcode b ON p.product_id = b.product_id WHERE p.sku LIKE ? AND b.barcode LIKE ? LIMIT ? OFFSET ?", sku, barcode, num, start)
 	if err != nil {
-		log.Fatal(err)
+		errorHandler(w, r, http.StatusInternalServerError)
+		// log.Fatal(err)
 	}
 	defer itemRes.Close()
 
@@ -70,17 +106,55 @@ func get(w http.ResponseWriter, r *http.Request) {
 		var item Item
 		err := itemRes.Scan(&item.ProductID, &item.Title)
 		if err != nil {
-			log.Fatal(err)
+			errorHandler(w, r, http.StatusInternalServerError)
+			// log.Fatal(err)
 		}
-		fmt.Println(item)
+		// fmt.Println(item)
 		items = append(items, item)
 	}
 
-	post.Items = items
+	getAllProducts.Items = items
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
-	json.NewEncoder(w).Encode(post)
-	// w.Write([]byte(`{"message": "get called"}`))
+	json.NewEncoder(w).Encode(getAllProducts)
+}
+
+func getProduct(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	result, err := db.Query("SELECT p.product_id, p.title, p.sku, b.barcode, a.name, a.value, p.price, p.created, p.last_updated FROM sitoo_test_assignment.product p LEFT JOIN sitoo_test_assignment.product_barcode b ON p.product_id = b.product_id LEFT JOIN sitoo_test_assignment.product_attribute a ON p.product_id = a.product_id WHERE p.product_id = ? ", params["id"])
+	if err != nil {
+		errorHandler(w, r, http.StatusInternalServerError)
+		return
+	}
+	defer result.Close()
+	var getProduct GetProduct
+	var barcodes []string
+	var barcode string
+	var attributes []Attribute
+	var attribute Attribute
+	i := 0
+	for result.Next() {
+		err := result.Scan(&getProduct.ProductID, &getProduct.Title, &getProduct.Sku, &barcode, &attribute.Name, &attribute.Value, &getProduct.Price, &getProduct.Created, &getProduct.LastUpdated)
+		if err != nil {
+			errorHandler(w, r, http.StatusInternalServerError)
+			return
+		}
+		if !HasElem(attributes, attribute) {
+			attributes = append(attributes, attribute)
+		}
+		if !HasElem(barcodes, barcode) {
+			barcodes = append(barcodes, barcode)
+		}
+		i++
+	}
+	if i == 0 {
+		errorHandler(w, r, http.StatusNotFound)
+		return
+	}
+	getProduct.Barcodes = barcodes
+	getProduct.Attributes = attributes
+	json.NewEncoder(w).Encode(getProduct)
 }
 
 func post(w http.ResponseWriter, r *http.Request) {
@@ -101,15 +175,24 @@ func delete(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte(`{"message": "delete called"}`))
 }
 
-func notFound(w http.ResponseWriter, r *http.Request) {
+func errorHandler(w http.ResponseWriter, r *http.Request, status int) {
 	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusNotFound)
-	w.Write([]byte(`{"errorText": "The error message"}`))
+	w.WriteHeader(status)
+	switch status {
+	case http.StatusNotFound:
+		w.Write([]byte(`{"404": "Page Not Found"}`))
+		break
+	case http.StatusInternalServerError:
+		w.Write([]byte(`"500": "Internal Server Error !"`))
+		break
+	default:
+		w.Write([]byte(`"500": "Internal Server Error !"`))
+	}
 }
 
 func main() {
 	db, err = sql.Open("mysql",
-		"root:rootroot@tcp(127.0.0.1:3306)/sitoo_test_assignment")
+		"root:rootroot@tcp(127.0.0.1:3306)/sitoo_test_assignment?parseTime=true&")
 	if err != nil {
 		log.Fatal(err)
 	} else {
@@ -119,10 +202,10 @@ func main() {
 
 	r := mux.NewRouter()
 	api := r.PathPrefix("/api/products").Subrouter()
-	api.HandleFunc("", get).Methods(http.MethodGet)
+	api.HandleFunc("", getProducts).Methods(http.MethodGet)
+	api.HandleFunc("/{id}", getProduct).Methods(http.MethodGet)
 	api.HandleFunc("", post).Methods(http.MethodPost)
 	api.HandleFunc("", put).Methods(http.MethodPut)
 	api.HandleFunc("", delete).Methods(http.MethodDelete)
-	api.HandleFunc("", notFound)
 	log.Fatal(http.ListenAndServe(":8080", r))
 }
